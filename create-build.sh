cd3ea0370a54144fb010aa8dd8d004a9dc5c5359
Matus Ujhelyi <ujhelyi.m@gmail.com>
2021-02-06 13:03:59 +0100

    add first steps

---
+++ create-build.sh
@@ -0,0 +1,4 @@
+
+source poky-dunfell/oe-init-build-env build
+cp ../poky-dunfell/meta-rpi/conf/local.conf.sample conf/local.conf
+cp ../poky-dunfell/meta-rpi/conf/bblayers.conf.sample conf/bblayers.conf
