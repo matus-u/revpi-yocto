b223bb30bded92f8e56a21edb276b8a95c05e874
Matus Ujhelyi <ujhelyi.m@gmail.com>
2021-02-26 21:46:29 +0100

    set envs

--- create-build.sh
+++ create-build.sh
@@ -3,3 +3,6 @@ source poky/poky-dunfell/oe-init-build-env build
 cp ../meta-own/conf/local.conf.sample conf/local.conf
 cp ../meta-own/conf/bblayers.conf.sample conf/bblayers.conf
 sed -i "s|HOME|$PWD/..|" conf/bblayers.conf
+
+export MACHINE=raspberrypi-cm
+export OETMP=raspberrypi-cm=$PWD/tmp
